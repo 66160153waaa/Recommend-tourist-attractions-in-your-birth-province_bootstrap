f104df795fbf3d8dc7edbf743305d2e88f5175b5
Wanchalerm <66160153@go.buu.ac.th>
2024-12-26 06:05:45 +0700

    add Bootstrap Carousel

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ function searchFunction() {
     const searchQuery = document.getElementById("search").value;
     alert("คุณกำลังค้นหาคำว่า: " + searchQuery);
 }
+
+function loadBootstrap(callback) {
+    const script = document.createElement("script");
+    script.src = "https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js";
+    script.onload = callback; // เรียก callback หลังโหลดเสร็จ
+    script.onerror = function () {
+        console.error("Failed to load Bootstrap.");
+    };
+    document.head.appendChild(script);
+}
