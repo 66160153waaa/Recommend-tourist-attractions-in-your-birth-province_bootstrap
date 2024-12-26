ad5e6ffd174d591ddc5feae3cd629099ae5ec4d9
Wanchalerm <66160153@go.buu.ac.th>
2024-12-26 07:59:07 +0700

    add modal

--- js/script.js
+++ js/script.js
@@ -19,3 +19,26 @@ function loadBootstrap(callback) {
     };
     document.head.appendChild(script);
 }
+
+// Get modal and buttons
+var modal = document.getElementById("contactModal");
+var openModalBtn = document.getElementById("openModalBtn");
+var closeModalBtn = document.getElementById("closeModalBtn");
+
+// Open the modal
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Close the modal
+closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close the modal if clicked outside of the modal content
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
